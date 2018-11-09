e44f37f33f3a7bd4abd53063e413ce1317c107af
ValmeI <ignarvalme@gmail.com>
2018-11-09 12:24:26 +0200

    cleanup

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -3,23 +3,16 @@ from Portfolio_calculator import Aktsiad
 morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 35,
                    "TKM1T": 53,
-                   "TSM1T": 560,
-                   "EXS1": 0.395,
-                   "EXSA": 19.482,
-                   "EXXT": 0.920,
-                   "SPYW": 5.629
+                   "TSM1T": 560
                    }
 
-morr_usa_stocks = {"SPYD": 1.214,
-                   "IVV": 7.076
-                   }
+'''morr_usa_stocks = {}'''
 
-ValCapitalRaha = 2260
+ValCapitalRaha = 2460
 
-m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True) + \
-            Aktsiad.stocks_value_combined(morr_usa_stocks, False)
+m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
-m_raha = 96.37
+m_raha = 2778.82
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
 
--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -4,7 +4,7 @@ from datetime import date
 
 fys_eur_stocks = {"OLF1R": 67,
                   "SFG1T": 1668,
-                  "TKM1T": 380,
+                  "TKM1T": 150,
                   "EFT1T": 66,
                   "TSM1T": 1000
                   }
@@ -22,7 +22,7 @@ jur_usa_stocks = {"AAPL": 15,
 Aka42_63_Laen_Kuupäev = date(2016, 2, 16)
 Aka38_20_Laen_Kuupäev = date(2017, 5, 9)
 
-FüsIsikRaha = 7.37
+FüsIsikRaha = 2015.14
 FysIsikAktsaid = Aktsiad.stocks_value_combined(fys_eur_stocks, True)
 
 '# Vaba raha ja aktsiad kokku'
@@ -31,7 +31,7 @@ FysIsik = round(FüsIsikRaha + FysIsikAktsaid)
 JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False)
 
 '#jur isiku raha LHV + LYNX RAHA'
-JurRaha = 1.39
+JurRaha = 211.28
 JurLynxRaha = 80
 JurIsik = round(JurRaha + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
 
