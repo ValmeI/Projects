3a4f99e4813992f4b3d6f341b2e03fb64f9f1013
ValmeI <ignarvalme@gmail.com>
2019-02-02 15:27:15 +0200

    Django Test

--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -37,7 +37,7 @@ FysIsik = round(FüsIsikRaha + FysIsikAktsaid)
 JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False) + Aktsiad.stocks_value_combined(jur_eur_stocks, True)
 
 '#jur isiku raha LHV + LYNX RAHA'
-JurRaha = 523.97
+JurRaha = 510.81
 JurLynxRaha = 8
 JurIsik = round(JurRaha + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
 
