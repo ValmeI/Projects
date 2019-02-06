5c8e475321f0d58a524a549087bc0ec671426d7e
ValmeI <ignarvalme@gmail.com>
2019-02-06 10:09:06 +0200

    Django Test

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -3,7 +3,12 @@ from Portfolio_calculator import Aktsiad
 morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 35,
                    "TKM1T": 53,
-                   "TSM1T": 560
+                   "TSM1T": 560,
+                   "EXS1": 0.093,
+                   "EXSA": 0.512,
+                   "EXXT": 0.308,
+                   "SPYD": 0.389,
+                   "SPYW": 1.25
                    }
 
 '''morr_usa_stocks = {}'''
@@ -15,4 +20,3 @@ m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 m_raha = 158.82
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
-
