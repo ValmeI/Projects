feabf96f8f044fe23730c6ce010f317ff5c805da
ValmeI <ignarvalme@gmail.com>
2019-02-19 18:13:10 +0200

    Django Test

--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -28,7 +28,7 @@ Vana_Aka38_20_Laen_Kuupäev = date(2017, 5, 9)
 Aka42_63_Laen_Kuupäev = date(2018, 12, 5)
 Aka38_20_Laen_Kuupäev = date(2018, 12, 5)
 
-FüsIsikRaha = 348.74
+FüsIsikRaha = 848.74
 FysIsikAktsaid = Aktsiad.stocks_value_combined(fys_eur_stocks, True)
 
 '# Vaba raha ja aktsiad kokku'
@@ -37,7 +37,7 @@ FysIsik = round(FüsIsikRaha + FysIsikAktsaid)
 JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False) + Aktsiad.stocks_value_combined(jur_eur_stocks, True)
 
 '#jur isiku raha LHV + LYNX RAHA'
-JurRaha = 1003.97
+JurRaha = 103.97
 JurLynxRaha = 8
 JurIsik = round(JurRaha + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
 
