de5ada98cb2bab47efef0e6179437338f23e4061
ValmeI <ignarvalme@gmail.com>
2019-02-24 14:42:32 +0200

    Django Test

--- Test_Machine_Learning/Calender.py
+++ Test_Machine_Learning/Calender.py
@@ -1,8 +1,17 @@
 import calendar
+import datetime
 
-tc = calendar.HTMLCalendar(firstweekday=0).formatmonth(2019, 3)
-print(tc)
+'# Create global variable to display the correct month'
+month_to_display = datetime.date.today().month
+year_to_display = datetime.date.today().year
+
+#print(year_to_display, month_to_display)
+
+
+tc = calendar.HTMLCalendar(firstweekday=0).formatmonth(year_to_display, month_to_display)
+#print(tc)
 
 
 file = open("calnder_source.html", "w+", encoding='utf-8')
 file.write(tc)
+
