79e742ab0ae95e36e4623e61bbd73589d9bc15fd
ValmeI <ignarvalme@gmail.com>
2019-04-09 17:13:06 +0300

    plot added to portfolio results webserver

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -4,11 +4,11 @@ morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 35,
                    "TKM1T": 53,
                    "TSM1T": 560,
-                   "EXS1": 0.153,
-                   "EXSA": 0.837,
-                   "EXXT": 0.502,
-                   "SPYD": 0.639,
-                   "SPYW": 2.043
+                   "EXS1": 0.278,
+                   "EXSA": 1.510,
+                   "EXXT": 0.893,
+                   "SPYD": 1.163,
+                   "SPYW": 3.730
                    }
 
 '''morr_usa_stocks = {}'''
@@ -17,7 +17,7 @@ ValCapitalRaha = 3670
 
 m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
-m_raha = 158.82
+m_raha = 94.71
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
 
