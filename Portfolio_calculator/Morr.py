3b634047872a17a797218aa357280512a09a1dcf
ValmeI <ignarvalme@gmail.com>
2019-07-12 12:32:39 +0300

    nr updates

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -4,11 +4,11 @@ morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 46,
                    "TKM1T": 53,
                    "TSM1T": 560,
-                   "EXS1": 0.496,
-                   "EXSA": 2.714,
-                   "EXXT": 1.595,
-                   "SPYD": 2.094,
-                   "SPYW": 6.701
+                   "EXS1": 0.589,
+                   "EXSA": 3.232,
+                   "EXXT": 1.893,
+                   "SPYD": 2.497,
+                   "SPYW": 7.983
                    }
 
 '''morr_usa_stocks = {}'''
@@ -17,7 +17,7 @@ ValCapitalRaha = 4295
 
 m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
-m_raha = 115.66
+m_raha = 115.96
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
 
