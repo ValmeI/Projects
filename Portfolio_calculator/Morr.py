881c50d3b993e1d51ccbcce91d9098aa468a5b20
ValmeI <ignarvalme@gmail.com>
2019-10-02 17:33:10 +0300

    nr updates

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -13,7 +13,7 @@ morr_eur_stocks = {"APG1L": 196,
 
 '''morr_usa_stocks = {}'''
 
-ValCapitalRaha = 4695
+ValCapitalRaha = 4895
 
 m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -39,7 +39,7 @@ FysIsik = round(FüsIsikRaha + FysIsikAktsaid)
 JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False) + Aktsiad.stocks_value_combined(jur_eur_stocks, True)
 
 '#jur isiku raha LHV + LYNX RAHA'
-JurRaha = 96.89
+JurRaha = 78.74
 JurFunderBeam = 1500
 JurLynxRaha = 8
 JurIsik = round(JurRaha + JurFunderBeam + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
