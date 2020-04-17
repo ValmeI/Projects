1055a7f058e9e7b42ee55a1f3d539f980c5555a5
ValmeI <ignarvalme@gmail.com>
2020-04-17 19:27:10 +0300

    nr updates

--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -38,7 +38,7 @@ JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False)
 
 '#jur isiku raha LHV + LYNX RAHA'
 JurRaha = 2500
-JurFunderBeam = 3100
+JurFunderBeam = 3400
 JurLynxRaha = 47
 JurIsik = round(JurRaha + JurFunderBeam + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
 '# Mörr on väike karu'
