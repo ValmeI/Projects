45dc3887189fad83a83bba73efb7d79701efe8db
ValmeI <ignarvalme@gmail.com>
2020-05-12 18:23:50 +0300

    nr updates

--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -37,8 +37,8 @@ FysIsik = round(FüsIsikRaha + FysIsikAktsaid)
 JurAktsiad = Aktsiad.stocks_value_combined(jur_usa_stocks, False)
 
 '#jur isiku raha LHV + LYNX RAHA'
-JurRaha = 2500
-JurFunderBeam = 3100
+JurRaha = 3200
+JurFunderBeam = 3300
 JurLynxRaha = 47
 JurIsik = round(JurRaha + JurFunderBeam + JurLynxRaha + JurAktsiad + Morr.ValCapitalRaha/2)
 '# Mörr on väike karu'
