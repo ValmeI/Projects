fb9cf5519c2401e9cc079d8bb0f20017f7247921
ValmeI <ignarvalme@gmail.com>
2020-08-13 17:25:06 +0300

    nr updates

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -17,7 +17,7 @@ morr_eur_stocks = {"APG1L": 196,
 
 Sõle_Laen_Kuupäev = date(2011, 8, 25)
 
-ValCapitalRaha = 3800
+ValCapitalRaha = 3400
 
 m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
