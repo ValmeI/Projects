61348be0f5830b1d63133f905496990abd0f23e7
ValmeI <ignarvalme@gmail.com>
2020-10-24 17:31:32 +0300

    nr updates

--- Portfolio_calculator/Valme.py
+++ Portfolio_calculator/Valme.py
@@ -26,7 +26,7 @@ Aka42_63_Laen_Kuupäev = date(2018, 12, 5)
 Aka38_20_Laen_Kuupäev = date(2018, 12, 5)
 Vilde90_193_Laen_Kuupäev = date(2019, 4, 9)
 
-FüsIsikRaha = 3274.98
+FüsIsikRaha = 2274.98
 FysIsikAktsaid = Aktsiad.stocks_value_combined(fys_eur_stocks, True)
 
 '# Vaba raha ja aktsiad kokku'
