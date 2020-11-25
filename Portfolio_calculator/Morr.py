b6a0165204bcc3686a0bba1a98b0acdc5fd94f96
ValmeI <ignarvalme@gmail.com>
2020-11-25 17:31:44 +0200

    nr updates

--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -6,21 +6,21 @@ morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 46,
                    "TKM1T": 53,
                    "TSM1T": 560,
-                   "EXS1": 2.039,
-                   "EXSA": 11.460,
-                   "EXXT": 5.768,
-                   "SPYD": 	8.902,
-                   "SPYW": 29.227
+                   "EXS1": 2.133,
+                   "EXSA": 12.012,
+                   "EXXT": 5.969,
+                   "SPYD": 	9.337,
+                   "SPYW": 30.792
                    }
 
 '''morr_usa_stocks = {}'''
 
 Sõle_Laen_Kuupäev = date(2011, 8, 25)
 
-ValCapitalRaha = 7100
+ValCapitalRaha = 7200
 
 m_aktsiad = Aktsiad.stocks_value_combined(morr_eur_stocks, True)
 
-m_raha = 228.11
+m_raha = 227
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
