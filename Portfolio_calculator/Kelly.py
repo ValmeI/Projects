600d2df1ada708ee1c53eaaee5d302adb7b65b56
ValmeI <ignarvalme@gmail.com>
2021-09-11 10:38:42 +0300

    bitcoin to euro easy google convert added

--- Portfolio_calculator/Kelly.py
+++ Portfolio_calculator/Kelly.py
@@ -6,8 +6,8 @@ ETH_amount = 0.10581
 ETH_USD = Aktsiad.crypto_price_from_coinmarketcap('ethereum') * ETH_amount
 ETH_EUR = Aktsiad.usd_to_eur_convert(ETH_USD)
 
-Kelly_raha = 50
-Kelly_Invest_raha = 43.87
+Kelly_raha = 0
+Kelly_Invest_raha = 200
 
 Kelly_Portfell_Kokku = round(Kelly_raha + Kelly_Invest_raha + ETH_EUR)
 
--- Portfolio_calculator/Morr.py
+++ Portfolio_calculator/Morr.py
@@ -5,21 +5,21 @@ morr_eur_stocks = {"APG1L": 196,
                    "EFT1T": 55,
                    "TKM1T": 53,
                    "TSM1T": 560,
-                   "EXS1": 2.864,
-                   "EXSA": 16.318,
-                   "EXXT": 7.627,
-                   "SPYD": 	12.775,
-                   "SPYW": 43.179
+                   "EXS1": 4.721,
+                   "EXSA": 26.917,
+                   "EXXT": 11.46,
+                   "SPYD": 	21.405,
+                   "SPYW": 74.074
                    }
 
 '''morr_usa_stocks = {}'''
 
 '''Sõle_Laen_Kuupäev = date(2011, 8, 25) #Müüdud 22.06.2021'''
 
-ValCapitalRaha = 8900
+ValCapitalRaha = 9000
 
 m_aktsiad = round(Aktsiad.stocks_value_combined(morr_eur_stocks, True))
 
-m_raha = 54160
+m_raha = 51660
 
 kokku = round(ValCapitalRaha/2 + m_raha + m_aktsiad)
