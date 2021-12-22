85803db76217c7912ad92a5c79d9052588030160
ValmeI <ignarvalme@gmail.com>
2021-12-22 17:24:25 +0200

    nr update

--- Portfolio_calculator/Kelly.py
+++ Portfolio_calculator/Kelly.py
@@ -7,7 +7,7 @@ jur_eur_stocks = {
 
 ETH_amount = 0.10581
 
-ETH_USD = Aktsiad.crypto_price_from_coinmarketcap('ethereum') * ETH_amount
+ETH_USD = Aktsiad.crypto_to_eur('Ethereum') * ETH_amount
 ETH_EUR = Aktsiad.usd_to_eur_convert(ETH_USD)
 
 Kelly_raha = 0
