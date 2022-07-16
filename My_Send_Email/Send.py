691f74dccec68b03fe214179c46240276f18d984
Ignar <Ignarvalme@gmail.com>
2022-07-16 13:22:27 +0300

    updates

--- My_Send_Email/Send.py
+++ My_Send_Email/Send.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.message import EmailMessage
+from email.utils import make_msgid
 from termcolor import colored
+from secrets import token_hex
 
 # copy-d it to C:\Users\YOURUSER\AppData\Local\Programs\Python\PythonXX-XX\Lib\site-packages
 
@@ -10,6 +12,9 @@ def send_email(stmp_variable, user, password_file, sent_from, sent_to, sent_subj
     server = smtplib.SMTP_SSL(stmp_variable, 465)
     '# get password form file, read only'
     open_file = open(password_file + ".txt", 'r')
+    # creates random ID for message-ID, for some reason gmail wants it
+    random_id = token_hex(16)
+
     for password in open_file:
 
         server.login(user, password)
@@ -18,18 +23,19 @@ def send_email(stmp_variable, user, password_file, sent_from, sent_to, sent_subj
         msg['From'] = sent_from
         msg['To'] = sent_to
         msg['Subject'] = sent_subject
+        msg['Message-ID'] = make_msgid(idstring=random_id)
         msg.set_type('text/html')
         msg.set_content(sent_body)
 
         server.send_message(msg)
-
         print(colored('\nMail Send Successfully', 'green'))
         server.quit()
 
-'''send_email('valme.noip.me',
-               'email',
-               'C:\PycharmProjects\Projects\My_Send_Email\synology_pass',
-               'email@valme.noip.me',
-               'ignarvalme@gmail.com',
-               'Portfelli seis: ',
-               'Tulemus')'''
+
+send_email(stmp_variable='valme.noip.me',
+           user='email',
+           password_file=r'D:\PycharmProjects\Projects\My_Send_Email\synology_pass',
+           sent_from='email@valme.noip.me',
+           sent_to='ignarvalme@gmail.com',
+           sent_subject='Portfelli seis: ',
+           sent_body='Tulemus')
